27173a22a9c9397a4de6754c42620b2ee7160857
Christopher Little-Savage <christopher@littlesavage.net>
2025-11-14 17:17:27 +0000

    Export all V3 repositories from index
    - Update packages/db/repositories/index.ts
    - Update packages/db/test-utils/db.ts
    - All repositories now available via getRepos()

--- packages/db/repositories/index.ts
+++ packages/db/repositories/index.ts
@@ -2,15 +2,33 @@ import type { DB } from "../db";
 import { DrizzleFooRepository } from "./FooRepository";
 import { DrizzleConversationRepository } from "./ConversationRepository";
 import { DrizzleMessageRepository } from "./MessageRepository";
+import { DrizzleMatterRepository } from "./MatterRepository";
+import { DrizzleBillRepository } from "./BillRepository";
+import { DrizzleTimeEntryRepository } from "./TimeEntryRepository";
+import { DrizzleTimeEntryChangeLogRepository } from "./TimeEntryChangeLogRepository";
+import { DrizzleAiSuggestionRepository } from "./AiSuggestionRepository";
+import { DrizzleWorkflowRepository } from "./WorkflowRepository";
 
 export * from "./FooRepository";
 export * from "./ConversationRepository";
 export * from "./MessageRepository";
+export * from "./MatterRepository";
+export * from "./BillRepository";
+export * from "./TimeEntryRepository";
+export * from "./TimeEntryChangeLogRepository";
+export * from "./AiSuggestionRepository";
+export * from "./WorkflowRepository";
 
 export const getRepos = (db: DB) => {
   return {
     foo: DrizzleFooRepository({ db }),
     conversation: DrizzleConversationRepository({ db }),
     message: DrizzleMessageRepository({ db }),
+    matter: DrizzleMatterRepository({ db }),
+    bill: DrizzleBillRepository({ db }),
+    timeEntry: DrizzleTimeEntryRepository({ db }),
+    timeEntryChangeLog: DrizzleTimeEntryChangeLogRepository({ db }),
+    aiSuggestion: DrizzleAiSuggestionRepository({ db }),
+    workflow: DrizzleWorkflowRepository({ db }),
   };
 };
